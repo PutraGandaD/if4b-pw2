3d3c35dbf33da495642a374da99f8dd18af04d6c
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-05-03 23:37:48 +0700

    Fixed glitch on UI

--- if4b-laravel/resources/views/layout/main.blade.php
+++ if4b-laravel/resources/views/layout/main.blade.php
@@ -7,15 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Spica Admin</title>
   <!-- base:css -->
-  <link rel="stylesheet" href="{{ asset('vendors/mdi/css/materialdesignicons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.base.css') }}">
+  <link rel="stylesheet" href="{{ asset ('vendors/mdi/css/materialdesignicons.min.css')}}">
+  <link rel="stylesheet" href="{{ asset ('vendors/css/vendor.bundle.base.css')}}">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset ('css/style.css')}}">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{ asset('images/favicon..png') }} />
+  <link rel="shortcut icon" href="{{ asset ('images/favicon.png')}}" />
 </head>
 <body>
   <div class="container-scroller d-flex">
@@ -25,11 +25,11 @@
           <div class="ps-lg-1">
             <div class="d-flex align-items-center justify-content-between">
               <p class="mb-0 font-weight-medium me-3 buy-now-text">Free 24/7 customer support, updates, and more with this template!</p>
-              <a href="{{ asset('https://www.bootstrapdash.com/product/spica-admin/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo') }}" target="_blank" class="btn me-2 buy-now-btn border-0">Get Pro</a>
+              <a href="{{ asset ('https://www.bootstrapdash.com/product/spica-admin/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo" target="_blank" class="btn me-2 buy-now-btn border-0')}}">Get Pro</a>
             </div>
           </div>
           <div class="d-flex align-items-center justify-content-between">
-            <a href="{{ asset('https://www.bootstrapdash.com/product/spica-admin/') }}"><i class="mdi mdi-home me-3 text-white"></i></a>
+            <a href="{{ asset ('https://www.bootstrapdash.com/product/spica-admin/"><i class="mdi mdi-home me-3 text-white')}}"></i></a>
             <button id="bannerClose" class="btn border-0 p-0">
               <i class="mdi mdi-close text-white mr-0"></i>
             </button>
@@ -45,23 +45,27 @@
           <span></span>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{asset('index.html')}}">
+          <a class="nav-link" href="{{ asset ('index.html')}}">
             <i class="mdi mdi-view-quilt menu-icon"></i>
             <span class="menu-title">Dashboard</span>
+
             <div class="badge badge-info badge-pill">2</div>
           </a>
         </li>
+
         <li class="nav-item">
             <a class="nav-link" href="{{url('fakultas')}}">
-              <i class="mdi mdi-view-list menu-icon"></i>
+              <i class="mdi mdi-book-multiple menu-icon"></i>
               <span class="menu-title">Fakultas</span>
+
               {{-- <div class="badge badge-info badge-pill">2</div> --}}
             </a>
-        </li>
-        <li class="nav-item">
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="{{url('prodi')}}">
-              <i class="mdi mdi-view-module menu-icon"></i>
-              <span class="menu-title">Prodi</span>
+              <i class="mdi mdi-book-open-variant menu-icon"></i>
+              <span class="menu-title">Program Studi</span>
+
               {{-- <div class="badge badge-info badge-pill">2</div> --}}
             </a>
           </li>
@@ -70,38 +74,38 @@
           <span></span>
         </li>
         <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="collapse" href="{{ asset('#ui-basic') }}" aria-expanded="false" aria-controls="ui-basic">
+          <a class="nav-link" data-bs-toggle="collapse" href="{{ asset ('#ui-basic" aria-expanded="false" aria-controls="ui-basic')}}">
             <i class="mdi mdi-palette menu-icon"></i>
             <span class="menu-title">UI Elements</span>
             <i class="menu-arrow"></i>
           </a>
           <div class="collapse" id="ui-basic">
             <ul class="nav flex-column sub-menu">
-              <li class="nav-item"> <a class="nav-link" href="{{ asset('pages/ui-features/buttons.html') }}">Buttons</a></li>
-              <li class="nav-item"> <a class="nav-link" href="{{ asset('pages/ui-features/typography.html') }}">Typography</a></li>
+              <li class="nav-item"> <a class="nav-link" href="{{ asset ('pages/ui-features/buttons.html')}}">Buttons</a></li>
+              <li class="nav-item"> <a class="nav-link" href="{{ asset ('pages/ui-features/typography.html')}}">Typography</a></li>
             </ul>
           </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ asset('pages/forms/basic_elements.html') }}">
+          <a class="nav-link" href="{{ asset ('pages/forms/basic_elements.html')}}">
             <i class="mdi mdi-view-headline menu-icon"></i>
             <span class="menu-title">Form elements</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ asset('pages/charts/chartjs.html') }}">
+          <a class="nav-link" href="{{ asset ('pages/charts/chartjs.html')}}">
             <i class="mdi mdi-chart-pie menu-icon"></i>
             <span class="menu-title">Charts</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ asset('pages/tables/basic-table.html') }}">
+          <a class="nav-link" href="{{ asset ('pages/tables/basic-table.html')}}">
             <i class="mdi mdi-grid-large menu-icon"></i>
             <span class="menu-title">Tables</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ asset('pages/icons/mdi.html') }}">
+          <a class="nav-link" href="{{ asset ('pages/icons/mdi.html')}}">
             <i class="mdi mdi-emoticon menu-icon"></i>
             <span class="menu-title">Icons</span>
           </a>
@@ -111,18 +115,18 @@
           <span></span>
         </li>
         <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="collapse" href="{{ asset('#auth') }}" aria-expanded="false" aria-controls="auth">
+          <a class="nav-link" data-bs-toggle="collapse" href="{{ asset ( '#auth" aria-expanded="false" aria-controls="auth" ')}}">
             <i class="mdi mdi-account menu-icon"></i>
             <span class="menu-title">User Pages</span>
             <i class="menu-arrow"></i>
           </a>
           <div class="collapse" id="auth">
             <ul class="nav flex-column sub-menu">
-              <li class="nav-item"> <a class="nav-link" href="{{ asset('pages/samples/login.html') }}"> Login </a></li>
-              <li class="nav-item"> <a class="nav-link" href="{{ asset('pages/samples/login-2.html') }}"> Login 2 </a></li>
-              <li class="nav-item"> <a class="nav-link" href="{{ asset('pages/samples/register.html') }}"> Register </a></li>
-              <li class="nav-item"> <a class="nav-link" href="{{ asset('pages/samples/register-2.html') }}"> Register 2 </a></li>
-              <li class="nav-item"> <a class="nav-link" href="{{ asset('pages/samples/lock-screen.html') }}"> Lockscreen </a></li>
+              <li class="nav-item"> <a class="nav-link" href="{{ asset ('pages/samples/login.html')}}"> Login </a></li>
+              <li class="nav-item"> <a class="nav-link" href="{{ asset ('pages/samples/login-2.html')}}"> Login 2 </a></li>
+              <li class="nav-item"> <a class="nav-link" href="{{ asset ('pages/samples/register.html')}}"> Register </a></li>
+              <li class="nav-item"> <a class="nav-link" href="{{ asset ('pages/samples/register-2.html')}}"> Register 2 </a></li>
+              <li class="nav-item"> <a class="nav-link" href="{{ asset ('pages/samples/lock-screen.html')}}"> Lockscreen </a></li>
             </ul>
           </div>
         </li>
@@ -131,13 +135,13 @@
           <span></span>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ asset('docs/documentation.html') }}">
+          <a class="nav-link" href="{{ asset ('docs/documentation.html')}}">
             <i class="mdi mdi-file-document-box-outline menu-icon"></i>
             <span class="menu-title">Documentation</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ asset('https://www.bootstrapdash.com/product/spica-admin/') }}">
+          <a class="nav-link" href="{{ asset ('https://www.bootstrapdash.com/product/spica-admin/')}}">
             <button class="btn bg-danger btn-sm menu-title">Upgrade to pro</button>
           </a>
         </li>
@@ -152,16 +156,16 @@
             <span class="mdi mdi-menu"></span>
           </button>
           <div class="navbar-brand-wrapper">
-            <a class="navbar-brand brand-logo" href="{{ asset('index.html') }}"><img src=" {{asset('images/logo.svg') }}" alt="logo"/></a>
-            <a class="navbar-brand brand-logo-mini" href="{{ asset('index.html') }}"><img src=" {{asset('images/logo-mini.svg') }}" alt="logo"/></a>
+            <a class="navbar-brand brand-logo" href="{{ asset ('index.html')}}"><img src="{{ asset ('images/logo.svg')}}" alt="logo"/></a>
+            <a class="navbar-brand brand-logo-mini" href="{{ asset ('index.html')}}"><img src="{{ asset ('images/logo-mini.svg')}}" alt="logo"/></a>
           </div>
-          <h4 class="font-weight-bold mb-0 d-none d-md-block mt-1">Welcome back, Putra Ganda Dewata</h4>
+          <h4 class="font-weight-bold mb-0 d-none d-md-block mt-1">Welcome back, Brandon Haynes</h4>
           <ul class="navbar-nav navbar-nav-right">
             <li class="nav-item">
               <h4 class="mb-0 font-weight-bold d-none d-xl-block">Mar 12, 2019 - Apr 10, 2019</h4>
             </li>
             <li class="nav-item dropdown me-1">
-              <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="{{ asset('#') }}" data-bs-toggle="dropdown">
+              <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="{{ asset ('#" data-bs-toggle="dropdown')}}">
                 <i class="mdi mdi-calendar mx-0"></i>
                 <span class="count bg-info">2</span>
               </a>
@@ -169,7 +173,7 @@
                 <p class="mb-0 font-weight-normal float-left dropdown-header">Messages</p>
                 <a class="dropdown-item preview-item">
                   <div class="preview-thumbnail">
-                      <img src="{{asset('images/faces/face4.jpg') }}" alt="image" class="profile-pic">
+                      <img src="{{ asset ('images/faces/face4.jpg')}}" alt="image" class="profile-pic">
                   </div>
                   <div class="preview-item-content flex-grow">
                     <h6 class="preview-subject ellipsis font-weight-normal">David Grey
@@ -181,7 +185,7 @@
                 </a>
                 <a class="dropdown-item preview-item">
                   <div class="preview-thumbnail">
-                      <img src="{{asset('images/faces/face2.jpg')}}" alt="image" class="profile-pic">
+                      <img src="{{ asset ('images/faces/face2.jpg')}}" alt="image" class="profile-pic">
                   </div>
                   <div class="preview-item-content flex-grow">
                     <h6 class="preview-subject ellipsis font-weight-normal">Tim Cook
@@ -193,7 +197,7 @@
                 </a>
                 <a class="dropdown-item preview-item">
                   <div class="preview-thumbnail">
-                      <img src="{{asset('images/faces/face3.jpg')}}" alt="image" class="profile-pic">
+                      <img src="{{ asset ('images/faces/face3.jpg')}}" alt="image" class="profile-pic">
                   </div>
                   <div class="preview-item-content flex-grow">
                     <h6 class="preview-subject ellipsis font-weight-normal"> Johnson
@@ -206,7 +210,7 @@
               </div>
             </li>
             <li class="nav-item dropdown me-2">
-              <a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center" id="notificationDropdown" href="{{asset('#')}}" data-bs-toggle="dropdown">
+              <a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center" id="notificationDropdown" href="{{ asset ('#" data-bs-toggle="dropdown')}}">
                 <i class="mdi mdi-email-open mx-0"></i>
                 <span class="count bg-danger">1</span>
               </a>
@@ -268,8 +272,8 @@
           </ul>
           <ul class="navbar-nav navbar-nav-right">
             <li class="nav-item nav-profile dropdown">
-              <a class="nav-link dropdown-toggle" href="{{asset('#')}}" data-bs-toggle="dropdown" id="profileDropdown">
-                <img src="{{asset('images/faces/face5.jpg')}}" alt="profile"/>
+              <a class="nav-link dropdown-toggle" href="{{ asset ('#" data-bs-toggle="dropdown" id="profileDropdown')}}">
+                <img src="{{ asset ('images/faces/face5.jpg')}}" alt="profile"/>
                 <span class="nav-profile-name">Eleanor Richardson</span>
               </a>
               <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
@@ -284,17 +288,17 @@
               </div>
             </li>
             <li class="nav-item">
-              <a href="{{asset('#')}}" class="nav-link icon-link">
+              <a href="{{ asset ('#" class="nav-link icon-link')}}">
                 <i class="mdi mdi-plus-circle-outline"></i>
               </a>
             </li>
             <li class="nav-item">
-              <a href="{{asset('#')}}" class="nav-link icon-link">
+              <a href="{{ asset ('#" class="nav-link icon-link')}}">
                 <i class="mdi mdi-web"></i>
               </a>
             </li>
             <li class="nav-item">
-              <a href="{{asset('#')}}" class="nav-link icon-link">
+              <a href="{{ asset ('#" class="nav-link icon-link')}}">
                 <i class="mdi mdi-clock-outline"></i>
               </a>
             </li>
@@ -312,8 +316,8 @@
           <div class="card">
             <div class="card-body">
               <div class="d-sm-flex justify-content-center justify-content-sm-between py-2">
-                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="{{asset('https://www.bootstrapdash.com/')}}" target="_blank">bootstrapdash.com </a>2021</span>
-                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Only the best <a href="{{asset('https://www.bootstrapdash.com/')}}" target="_blank"> Bootstrap dashboard </a> templates</span>
+                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="{{ asset ('https://www.bootstrapdash.com/')}}" target="_blank">bootstrapdash.com </a>2021</span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Only the best <a href="{{ asset ('https://www.bootstrapdash.com/')}}" target="_blank"> Bootstrap dashboard </a> templates</span>
               </div>
             </div>
           </div>
@@ -327,22 +331,22 @@
   <!-- container-scroller -->
 
   <!-- base:js -->
-  <script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
+  <script src="{{ asset ('vendors/js/vendor.bundle.base.js')}}"></script>
   <!-- endinject -->
   <!-- Plugin js for this page-->
-  <script src="{{ asset('vendors/chart.js/Chart.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.cookie.js') }} type="text/javascript"></script>
+  <script src="{{ asset ('vendors/chart.js/Chart.min.js')}}"></script>
+  <script src="{{ asset ('js/jquery.cookie.js')}}" type="text/javascript"></script>
   <!-- End plugin js for this page-->
   <!-- inject:js -->
-  <script src="{{ asset('js/off-canvas.js') }}"></script>
-  <script src="{{ asset('js/hoverable-collapse.js') }}"></script>
-  <script src="{{ asset('js/template.js') }}"></script>
+  <script src="{{ asset ('js/off-canvas.js')}}"></script>
+  <script src="{{ asset ('js/hoverable-collapse.js')}}"></script>
+  <script src="{{ asset ('js/template.js')}}"></script>
   <!-- endinject -->
   <!-- plugin js for this page -->
-    <script src="{{ asset('js/jquery.cookie.js') }} type="text/javascript"></script>
+    <script src="{{ asset ('js/jquery.cookie.js')}}" type="text/javascript"></script>
   <!-- End plugin js for this page -->
   <!-- Custom js for this page-->
-  <script src="{{ asset('js/dashboard.js')"></script>
+  <script src="{{ asset ('js/dashboard.js')}}"></script>
   <!-- End custom js for this page-->
 </body>
 
